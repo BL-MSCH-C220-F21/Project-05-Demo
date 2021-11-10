7659946500ba42466039ffa0b4f47a24f170e6e7
Jason Francis <88434199+BL-MSCH-C220-F21@users.noreply.github.com>
2021-11-10 16:45:37 -0500

    Demo completed

--- Player/Player.gd
+++ Player/Player.gd
@@ -10,28 +10,46 @@ var mouse_sensitivity = 0.002
 var mouse_range = 1.2
 var velocity = Vector2.ZERO
 
+var controlled = false 
+
+var id = 0
+
+var damage = 10
+var health = 100
+var max_health = 100
+
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	$Pivot/Camera.current = true
+	if (name == "Player1" and Global.which_player == 1) or (name == "Player2" and Global.which_player == 2):
+		controlled = true
+		$Pivot/Camera.current = true
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	$MeshInstance.set_surface_material(0, $MeshInstance.get_surface_material(0).duplicate())
+	if name == "Player1":
+		$MeshInstance.get_surface_material(0).albedo_color = Color8(34, 139, 230)
+	else:
+		$MeshInstance.get_surface_material(0).albedo_color = Color8(250, 82,82, 230)	
 
 func _physics_process(_delta):
-	velocity = get_input()*speed
-	velocity.y += gravity
-	if is_on_floor():
-		velocity.y = 0
+	if controlled:
+		velocity = get_input()*speed
+		velocity.y += gravity
+		if is_on_floor():
+			velocity.y = 0
 
-	if velocity != Vector3.ZERO:
-		velocity = move_and_slide(velocity, Vector3.UP)
-	
-	if Input.is_action_pressed("shoot"):
-		shoot()
+		if velocity != Vector3.ZERO:
+			velocity = move_and_slide(velocity, Vector3.UP)
+			rpc_unreliable("_set_position", global_transform.origin)
+		
+		if Input.is_action_pressed("shoot"):
+			shoot()
 
 func _input(event):
-	if event is InputEventMouseMotion:
+	if controlled and event is InputEventMouseMotion:
 		$Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
 		rotate_y(-event.relative.x * mouse_sensitivity)
 		$Pivot.rotation.x = clamp($Pivot.rotation.x, -mouse_range, mouse_range)
+		rpc_unreliable("_set_rotation", rotation, $Pivot.rotation)
 
 func get_input():
 	var input_dir = Vector3.ZERO
@@ -50,6 +68,7 @@ func shoot():
 	if not $Pivot/Flash.visible:
 		$Pivot/Flash.show()
 		$Pivot/Flash/Timer.start()
+		var target = ""
 		if $Pivot/RayCast.is_colliding():
 			var t = $Pivot/RayCast.get_collider()
 			var p = $Pivot/RayCast.get_collision_point()
@@ -58,7 +77,38 @@ func shoot():
 			t.add_child(decal)
 			decal.global_transform.origin = p
 			decal.look_at(p + n, Vector3.UP)
+			if t.has_method("damaged"):
+				target = t.get_path()
+				t.damaged(damage)
+		rpc_unreliable("_shoot", target, damage)
 
-	
+func damaged(d):
+	health -= d
+	$HealthBar.update(health, max_health)
+	if health <= 0:
+		if controlled:
+			var _scene = get_tree().change_scene("res://UI/Die.tscn")
+		else:
+			var _scene = get_tree().change_scene("res://UI/Win.tscn")
+		get_node("/root/Game").hide()
+			
+
+
+remote func _set_position(pos):
+	if not controlled:
+		global_transform.origin = pos
+
+remote func _set_rotation(rot, piv_rot):
+	if not controlled:
+		rotation = rot
+		$Pivot.rotation = piv_rot
+
+remote func _shoot(target, d):
+	if not controlled:
+		$Pivot/Flash.show()
+		$Pivot/Flash/Timer.start()
+		if target != "":
+			get_node(target).damaged(d)
+
 func _on_Timer_timeout():
 	$Pivot/Flash.hide()
